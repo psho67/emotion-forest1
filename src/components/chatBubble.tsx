76303652b8635c23c53db03a1996c217cab14ca1
psho67 <psho67@naver.com>
2025-04-16 22:39:11 +0900

    Update chatBubble.tsx

--- src/components/chatBubble.tsx
+++ src/components/chatBubble.tsx
@@ -1,19 +1,9 @@
-import React from 'react';
-import './chatBubble.css';
-
-interface ChatBubbleProps {
-  message: string;
-  isUser?: boolean;
-}
-
 function ChatBubble({ message, isUser = false }: ChatBubbleProps) {
   return (
     <div className={`chat-bubble-container ${isUser ? 'user' : 'other'}`}>
-      <div className={`chat-bubble ${isUser ? 'chat-bubble_user' : ''}`}>
+      <div className={`chat-bubble${isUser ? ' chat-bubble_user' : ''}`}>
         {message}
       </div>
     </div>
   );
 }
-
-export default ChatBubble;

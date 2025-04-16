ca864dc5a3062b3a89ae366a9cfc38af0aeb4e21
psho67 <psho67@naver.com>
2025-04-16 23:40:37 +0900

    Update chatBubble.tsx

--- src/components/chatBubble.tsx
+++ src/components/chatBubble.tsx
@@ -6,10 +6,10 @@ interface ChatBubbleProps {
   isUser?: boolean;
 }
 
-function chatBubble({ message, isUser = false }: ChatBubbleProps) {
+function ChatBubble({ message, isUser = false }: ChatBubbleProps) {
   return (
-    <div className={`chat-bubble-container ${isUser ? 'user' : 'other'}`}>
-      <div className={`chat-bubble${isUser ? ' Chat-bubble_user' : ''}`}>
+    <div className={`Chat-bubble-container ${isUser ? 'user' : 'other'}`}>
+      <div className={`Chat-bubble${isUser ? ' Chat-bubble_user' : ''}`}>
         {message}
       </div>
     </div>
